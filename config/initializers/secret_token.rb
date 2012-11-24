81c897fd33c14a80d5af53510dec33a31a600205
djrobd <djrobd@aol.com>
2012-11-23 20:06:50 -0500

    Created initial Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockChecker::Application.config.secret_token = '74be41cefd14e526e8679ca16c339994645fe1f739bfb2eaea4b8e01790070363f73a17acfd2f6468a995ea6121ea6c4edb92c7eb4f10f724bdd6ff624378b84'
